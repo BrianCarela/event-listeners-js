4ea59f6bd8dd4e9df51990551eb70df61555dff6
Brian Carela <BrianCarela@gmail.com>
2018-02-22 18:35:58 -0500

    everythings planned out, work starting

--- demonstrate/functions.js
+++ demonstrate/functions.js
@@ -1,2 +1,5 @@
 // 3 types of event listeners: click, hover, keyboard button. Don't let this get too complicated
 
+document.getElementById("zone-1").addEventListener("click", function(){
+  console.log("first test");
+});
