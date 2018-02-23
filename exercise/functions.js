f8296414601a25cc9f0a8e78589b3f9e6591def3
Brian Carela <BrianCarela@gmail.com>
2018-02-23 02:51:35 -0500

    my original exercise is garbage and convoluted, i have 5 minutes

--- exercise/functions.js
+++ exercise/functions.js
@@ -1,4 +1,5 @@
 // Starter Variables
+var zones = document.getElementByClassName("zone");
 var zone1 = document.getElementById("zone-1");
 var zone2 = document.getElementById("zone-2");
 var zone3 = document.getElementById("zone-3");
@@ -6,4 +7,25 @@ var zone4 = document.getElementById("zone-4");
 
 var all = [zone1, zone2, zone3, zone4];
 
-console.log('length of things ', arr.length);
+console.log('number of zones available: ', all.length);
+
+// Event Listeners don't always use a callback function, they can use functions we write beforehand.
+// This can help us remove Event Listeners
+
+function clicker(element){
+  element.addEventListener("click" function(){
+    element.classList.add('green');
+     all.pop();
+     // wow, come back to this... I need to remove event listeners individually
+     zones.removeEventListener("click", );
+     winner();
+     binder();
+   });
+}
+
+function binder(){
+  console.log('test');
+}
+
+// This begins binding the Event Listeners
+binder();
