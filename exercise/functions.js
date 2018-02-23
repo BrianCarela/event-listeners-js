3fdd1339cfbf2c4699df891eb7f4fb826252ba04
Brian Carela <BrianCarela@gmail.com>
2018-02-23 02:23:13 -0500

    fun is over, exercise time

--- exercise/functions.js
+++ exercise/functions.js
@@ -3,3 +3,7 @@ var zone1 = document.getElementById("zone-1");
 var zone2 = document.getElementById("zone-2");
 var zone3 = document.getElementById("zone-3");
 var zone4 = document.getElementById("zone-4");
+
+var all = [zone1, zone2, zone3, zone4];
+
+console.log('length of things ', arr.length);
